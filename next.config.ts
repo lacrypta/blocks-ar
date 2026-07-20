0cf244b5a8f283e1222a4165f051a14547b31235
Agustin Kassis <webmaster@masize.com>
2026-07-20 18:37:20 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  return [
+    { source: '/.well-known/:path*', destination: 'https://lawallet.blocks.ar/.well-known/:path*' },
+  ]
 };
 
 export default nextConfig;
